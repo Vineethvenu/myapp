d72767a4a86bdda48679049c6aa0c2dc36ccdce2
vineethmafil <vineethmafil@gmail.com>
2024-09-23 05:34:40 +0000

    done

--- lib/features/login/view/loginPage.dart
+++ lib/features/login/view/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/core/utils/configs/styles/colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,99 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        color: AppColor.whiteTxt,
+        height: h,
+        width: w,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+
+  Widget _customTextField({
+    required String labelTxt,
+    required String hintTxt,
+    required TextEditingController? controller,
+    required TextInputType? keyboardType,
+    required TextStyle? labelTxtStyle,
+    required TextStyle? hintTxtStyle,
+    String? Function(String?)? validator,
+    bool obscureText = false,
+    IconButton? suffixIcon,
+    bool readOnly = false,
+  }) {
+    return TextFormField(
+      controller: controller,
+      readOnly: readOnly,
+      validator: validator,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        filled: true,
+        fillColor: const Color(0xfff5f5f5),
+        suffixIcon: suffixIcon,
+        hintText: hintTxt,
+        hintStyle: hintTxtStyle,
+        contentPadding: const EdgeInsets.all(12),
+        labelText: labelTxt,
+        labelStyle: labelTxtStyle,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: AppColor.borderColor),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: AppColor.borderColor),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: AppColor.errorBorder),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+
+  Widget _customBtn({
+    required double width,
+    required double height,
+    required String text,
+    required Function() onPressed,
+  }) {
+    return Material(
+      color: Colors.transparent,
+      borderRadius: BorderRadius.circular(30),
+      elevation: 2,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(30),
+        onTap: onPressed,
+        splashColor: Colors.black.withOpacity(0.3),
+        child: Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: AppColor.btnColor,
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(color: AppColor.whiteTxt, fontSize: 14),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
